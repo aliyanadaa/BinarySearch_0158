ecefc085911780f28f8e0be4bbd9b654db7609f2
aliyanadaa <aliyanadaq@gmail.com>
2026-04-17 15:39:59 +0700

    validasi jumlah elemen

--- binarySearch.cpp/BinarySearch.cpp
+++ binarySearch.cpp/BinarySearch.cpp
@@ -7,5 +7,18 @@ int x;
 
 void input()
 {
-    
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10) :";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi. \n";
+        }
+
 }
