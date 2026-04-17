174b6d07e1698f11d22fce7d8c980a89adbcf32e
aliyanadaa <aliyanadaq@gmail.com>
2026-04-17 15:41:28 +0700

    header tampilan input

--- binarySearch.cpp/BinarySearch.cpp
+++ binarySearch.cpp/BinarySearch.cpp
@@ -21,6 +21,10 @@ void input()
             cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi. \n";
         }
     }
+    cout << "\n=================================\n";
+    cout << "       Masukkan Elemen Array        \n";
+    cout << "====================================\n";
+
     for (int i = 0; i < nPanjang; i++)
     {
         cout << "Data ke-" << (i+1)<< " = ";
