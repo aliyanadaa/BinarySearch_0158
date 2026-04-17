b89ac5729720d4bc435c50fe678ce641b6cbc54f
aliyanadaa <aliyanadaq@gmail.com>
2026-04-17 15:40:57 +0700

    input elemen array

--- binarySearch.cpp/BinarySearch.cpp
+++ binarySearch.cpp/BinarySearch.cpp
@@ -20,5 +20,12 @@ void input()
         {
             cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi. \n";
         }
+    }
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i+1)<< " = ";
+        cin >> element[i];
+    }
+
 
 }
